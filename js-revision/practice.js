2bf7806ce025a56d3a9855eaa0ccb3b1b9befa48
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2024-01-14 16:29:12 +0530

    practice

---
+++ js-revision/practice.js
@@ -0,0 +1,31 @@
+//* Datatypes
+// let state = null;
+
+// console.log(state);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+//* Type conversion
+let score = "33abc";
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valInNum = Number(score)
+console.log(typeof valInNum)
+console.log(valInNum)
+
+//? "33" => 33
+//? "33abc" => NaN
+//? "null" => 0
+//? true => 1
+//? "asd" => NaN
+//? undefined => NaN
+
+let isloggedIn = 1
+let boolisLoggedIn = Boolean(isloggedIn)
+console.log(boolisLoggedIn)
+
+//? 1 => true
+//? "" => false
+//? "asrar" => true
